871b73b66d7fb21f202bff04a00678c1bfceff2d
trongminh20 <trongminh20@gmail.com>
2021-11-08 01:01:31 -0800

    done with mobile styling

--- src/Components/Boost.jsx
+++ src/Components/Boost.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { btnHover, btnMouseLeave } from './mouseEvents';
 export default function Boost() {
+    const [bg, setBg] = useState("");
+
+    const setBackground = () => {
+        if (window.innerWidth < 800) {
+            setBg("./images/bg-boost-mobile.svg");
+        } else if (window.innerWidth >= 800) {
+            setBg("./images/bg-boost-desktop.svg");
+        }
+    }
+    useEffect(() => {
+        window.addEventListener('resize', setBackground());
+        return () => {
+            window.removeEventListener('resize', setBackground());
+        }
+    }, [])
     return <section className="boost">
-        <img className="boost-bg" src="./images/bg-boost-desktop.svg" alt="" />
+        <img className="boost-bg" src={bg} alt="" />
         <div className="boost-info">
             <h1>Boost your links today</h1>
             <button className="btn boost-btn" onMouseOver={btnHover} onMouseLeave={btnMouseLeave}>Get Started!</button>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { logo } from './SVG-images';
 import { handleMouseLeave, handleHover, btnMouseLeave, btnHover } from './mouseEvents';
+
 export default function Navbar() {
+
+    const showNavMenu = e => {
+        if (document.getElementById('nav').style.display === "none") {
+            document.getElementById('nav').style.display = "flex";
+            console.log(document.getElementById('nav').style.display);
+        } else {
+            document.getElementById('nav').style.display = "none";
+            console.log(document.getElementById('nav').style.display);
+        }
+    }
+
+    const handleResponsiveNav = () => {
+        if (window.innerWidth < 800) {
+            document.getElementById('nav').style.display = "none";
+        } else if (window.innerWidth >= 800) {
+            document.getElementById('nav').style.display = "flex";
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResponsiveNav());
+        return () => {
+            window.removeEventListener('resize', handleResponsiveNav());
+        }
+    }, [])
 
     return <nav className="navbar">
         <Link to="/">
@@ -10,7 +36,7 @@ export default function Navbar() {
                 {logo}
             </div>
         </Link>
-        <div className="nav">
+        <div className="nav" id="nav">
             <div className="nav-menu">
                 <Link to="/features">
                     <p onMouseOver={handleHover} onMouseLeave={handleMouseLeave}>Features</p>
@@ -24,14 +50,14 @@ export default function Navbar() {
             </div>
             <div className="nav-btns">
                 <Link to="/login">
-                    <button className="btn login-btn" onMouseOver={handleHover} onMouseLeave={handleMouseLeave}>Login</button>
+                    <p className="login-btn" onMouseOver={handleHover} onMouseLeave={handleMouseLeave}>Login</p>
                 </Link>
                 <Link to="/signup">
                     <button className="btn signup-btn" onMouseOver={btnHover} onMouseLeave={btnMouseLeave}>Sign Up</button>
                 </Link>
             </div>
         </div>
-        <div className="responsive-btn">
+        <div onClick={() => showNavMenu()} className="responsive-btn">
             <i className="fas fa-bars"></i>
         </div>
     </nav>
